0b45ee91be9d47f626482e59414aeb6eb7dcc622
曲腾飞 <qutengfei@shopin.cn>
2017-05-28 15:19:54 +0800

    framework init

---
+++ bigBoss/bigBoss-web/bigBoss-web-api/src/main/java/net/home/proj/bigBossApplication.java
@@ -0,0 +1,15 @@
+package net.home.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by leo on 2017/5/28.
+ */
+@SpringBootApplication
+public class bigBossApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(bigBossApplication.class, args);
+    }
+}
---
+++ bigBoss/bigBoss-web/bigBoss-web-api/target/bigBoss-web-api/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! come in</h2>
+</body>
+</html>
